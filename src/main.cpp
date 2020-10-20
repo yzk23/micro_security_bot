21df66110c8058c2740fcc8ada01b371bb2e5cfd
Isaac Medeiros <isaacoelhomedeiros@gmail.com>
2020-10-20 02:26:36 -0300

    deu bom!

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,18 @@
 
 #define timeSeconds 3
 
+const char* SSID = "";
+const char* PASSWORD = "";
+
+#define BOT_TOKEN ""
+#define CHAT_ID ""
+
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOT_TOKEN, client);
+
+int botRequestDelay = 1000;
+unsigned long lastTimeBotRan;
+
 const int led = 2;
 const int motionSensor = 27;
 
@@ -28,12 +40,24 @@ void setup() {
 
   pinMode(led, OUTPUT);
   digitalWrite(led, LOW);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID, PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
   now = millis();
   
   if (startTimer && (now - lastTrigger > (timeSeconds * 1000))) {
+    bot.sendMessage(CHAT_ID, "Alguém entrou na sua casa", "");
     Serial.println("Movimentação parada.");
     digitalWrite(led, LOW);
     startTimer = false;
