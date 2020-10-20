e57e33541239f994da13962817704cc880982a3d
Isaac Medeiros <isaacoelhomedeiros@gmail.com>
2020-10-19 23:36:18 -0300

    Hello, PIR Sensor!

--- src/main.cpp
+++ src/main.cpp
@@ -3,94 +3,39 @@
 #include <UniversalTelegramBot.h>
 #include <ArduinoJson.h>
 
-const char* SSID = "NOME_DA_REDE";
-const char* PASSWORD = "SENHA_DA_REDE";
+#define timeSeconds 3
 
-#define BOT_TOKEN "TOKEN"
-#define CHAT_ID "CHAT_ID"
+const int led = 2;
+const int motionSensor = 27;
 
-WiFiClientSecure client;
-UniversalTelegramBot bot(BOT_TOKEN, client);
+unsigned long now = millis();
+unsigned long lastTrigger = 0;
+boolean startTimer = false;
 
-int botRequestDelay = 1000;
-unsigned long lastTimeBotRan;
-
-const int ledPin = 2;
-bool ledState = LOW;
-
-void handleNewMessages(int numNewMessages) {
-  Serial.println("handleNewMessages");
-  Serial.println(String(numNewMessages));
-
-  for (int i = 0; i < numNewMessages; i++) {
-    String chat_id = String(bot.messages[i].chat_id);
-    if (chat_id != CHAT_ID){
-      bot.sendMessage(chat_id, "Unauthorized user", "");
-      continue;
-    }
-    
-    String text = bot.messages[i].text;
-    Serial.println(text);
-
-    String from_name = bot.messages[i].from_name;
-
-    if (text == "/start") {
-      String welcome = "Welcome, " + from_name + ".\n";
-      welcome += "Use the following commands to control your outputs.\n\n";
-      welcome += "/led_on to turn GPIO ON \n";
-      welcome += "/led_off to turn GPIO OFF \n";
-      welcome += "/state to request current GPIO state \n";
-      bot.sendMessage(chat_id, welcome, "");
-    }
-
-    if (text == "/led_on") {
-      bot.sendMessage(chat_id, "LED state set to ON", "");
-      ledState = HIGH;
-      digitalWrite(ledPin, ledState);
-    }
-    
-    if (text == "/led_off") {
-      bot.sendMessage(chat_id, "LED state set to OFF", "");
-      ledState = LOW;
-      digitalWrite(ledPin, ledState);
-    }
-    
-    if (text == "/state") {
-      if (digitalRead(ledPin)){
-        bot.sendMessage(chat_id, "LED is ON", "");
-      }
-      else{
-        bot.sendMessage(chat_id, "LED is OFF", "");
-      }
-    }
-  }
+void IRAM_ATTR detectsMovement() {
+  Serial.println("Movimentação detectada!");
+  digitalWrite(led, HIGH);
+  startTimer = true;
+  lastTrigger = millis();
 }
 
 void setup() {
   Serial.begin(115200);
+  
+  pinMode(motionSensor, INPUT_PULLUP);
+  
+  attachInterrupt(digitalPinToInterrupt(motionSensor), detectsMovement, RISING);
 
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, ledState);
-  
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(SSID, PASSWORD);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
-  }
-  
-  Serial.println(WiFi.localIP());
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);
 }
 
 void loop() {
-  if (millis() > lastTimeBotRan + botRequestDelay)  {
-    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
-
-    while(numNewMessages) {
-      Serial.println("got response");
-      handleNewMessages(numNewMessages);
-      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
-    }
-    lastTimeBotRan = millis();
+  now = millis();
+  
+  if (startTimer && (now - lastTrigger > (timeSeconds * 1000))) {
+    Serial.println("Movimentação parada.");
+    digitalWrite(led, LOW);
+    startTimer = false;
   }
 }
